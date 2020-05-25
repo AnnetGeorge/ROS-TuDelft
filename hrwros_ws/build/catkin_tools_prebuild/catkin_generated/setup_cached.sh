9c7f00cc7be821ce799b12264eddcdb116fde17d
annetbge <annetbge@umich.edu>
2020-05-25 16:12:26 -0400

    week 2

--- hrwros_ws/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ hrwros_ws/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/annet/ros/hrwros_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/melodic/lib:/.singularity.d/libs"
+export PKG_CONFIG_PATH="/opt/ros/melodic/lib/pkgconfig"
 export PWD="/home/annet/ros/hrwros_ws/build/catkin_tools_prebuild"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/annet/ros/hrwros_ws/devel/.private/catkin_tools_prebuild/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/annet/ros/hrwros_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
+export PYTHONPATH="/opt/ros/melodic/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/annet/ros/hrwros_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/annet/ros/hrwros_ws/build/catkin_tools_prebuild:/home/annet/ros/hrwros_ws/src/hrwros/hrwros_msgs:/home/annet/ros/hrwros_ws/src/hrwros_support:/home/annet/ros/hrwros_ws/src/hrwros/hrwros_week1:/home/annet/ros/hrwros_ws/src/hrwros_week2:/opt/ros/melodic/share"
